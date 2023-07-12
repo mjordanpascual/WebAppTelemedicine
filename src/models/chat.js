0eeaaed9bbe782ed58f09092a0c2a7b7dfb58f61
mjordanpascual <mjordan_pascual@yahoo.com>
2023-07-12 15:54:47 +0800

    Setup Send Chat

--- src/models/chat.js
+++ src/models/chat.js
@@ -1,7 +1,5 @@
-import { doc, onSnapshot } from "firebase/firestore";
-
-
-
+import { addDoc, collection } from 'firebase/firestore'
+import { db } from 'boot/firebaseConnection'
 
 const allChats = () => {
   return JSON.parse(localStorage.getItem("chats") || "[]");
@@ -21,23 +19,16 @@ export const getChats = (user_id) => {
   });
 };
 
-export const createChat = (text, sender, receiver) => {
-  return new Promise((resolve) => {
-    setTimeout(async () => {
-      let chats = allChats();
-      let data = {
-        text,
-        sender_id: sender.id,
-        receiver_id: receiver.id,
-        sender_name: sender.name,
-        receiver_name: receiver.name,
-        id: chats.length + 1,
-      };
+export const createChat = async (text, sender, receiver) => {
+  let data = {
+    text,
+    sender_id: sender.id,
+    receiver_id: receiver.id,
+    sender_name: sender.name,
+    receiver_name: receiver.name
+  };
 
-      chats.push(data);
-      localStorage.setItem("chats", JSON.stringify(chats));
+  const docRef = await addDoc(collection(db, "chats"), data);
 
-      resolve(data);
-    }, 1000);
-  });
+  return docRef.id;
 };
