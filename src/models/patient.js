3e611d09bb92de54e77e36287f0de0e2ad556eda
mjordanpascual <mjordan_pascual@yahoo.com>
2023-07-12 16:30:43 +0800

    update --host configuration

--- src/models/patient.js
+++ src/models/patient.js
@@ -1,184 +1,6 @@
-// import CustomInput from "components/CustomInput.vue";
-// import CustomSelect from "components/CustomSelect.vue";
-// import {
-//   requiredValidator,
-//   phoneValidator,
-//   nameValidator,
-//   dateValidator,
-//   pastDateValidator,
-// } from "utils/validators";
-// import { createInputFields, createTableColumns } from "models/functions";
-// import { emailValidator } from "src/utils/validators";
-
-
-// import { collection, addDoc, getDocs, doc, setDoc, deleteDoc } from "firebase/firestore";
-// import { db } from 'src/boot/firebaseConnection'
-
-
-
-// const model = [
-//   {
-//     component: CustomInput,
-//     model: "last_name",
-//     attrs: {
-//       label: "Last Name",
-//       rules: [requiredValidator, nameValidator],
-//     },
-//     col: 6,
-//   },
-//   {
-//     component: CustomInput,
-//     model: "first_name",
-//     attrs: {
-//       label: "First Name",
-//       rules: [requiredValidator, nameValidator],
-//     },
-//     col: 6,
-//   },
-//   {
-//     component: CustomInput,
-//     model: "middle_name",
-//     attrs: {
-//       label: "Middle Name",
-//       rules: [nameValidator],
-//     },
-//     col: 6,
-//   },
-//   {
-//     component: CustomSelect,
-//     model: "suffix_name",
-//     attrs: {
-//       label: "Suffix Name",
-//       options: ["Jr.", "Sr.", "I", "II", "III", "IV", "V"],
-//     },
-//     col: 6,
-//   },
-//   {
-//     component: CustomSelect,
-//     model: "sex",
-//     attrs: {
-//       label: "Sex",
-//       options: [
-//         { label: "Male", value: "M" },
-//         { label: "Female", value: "F" },
-//       ],
-//       rules: [requiredValidator],
-//       emitValue: true,
-//       mapOptions: true,
-//     },
-//     col: 6,
-//     format: (val) => (val === "M" ? "Male" : "Female"),
-//   },
-//   {
-//     component: CustomInput,
-//     model: "birth_date",
-//     attrs: {
-//       label: "Date of Birth",
-//       mask: "####-##-##",
-//       rules: [requiredValidator, dateValidator, pastDateValidator],
-//       placeholder: "YYYY-MM-DD",
-//     },
-//     col: 6,
-//     format: (val) => (val ? new Date(val).toDateString() : ""),
-//   },
-//   {
-//     component: CustomInput,
-//     model: "contact",
-//     attrs: {
-//       label: "Contact Number",
-//       mask: "###########",
-//       rules: [requiredValidator, phoneValidator],
-//       placeholder: "09123456789",
-//     },
-//     col: 6,
-//   },
-//   {
-//     component: CustomInput,
-//     model: "email",
-//     attrs: {
-//       label: "Email Address",
-//       rules: [emailValidator],
-//     },
-//     col: 6,
-//   },
-// ];
-
-// export const createFields = (overrides = []) => createInputFields(model, overrides);
-
-// export const createColumns = () => createTableColumns(model);
-
-// export const getPatients = async () => {
-
-//   const querySnapshot = await getDocs(collection(db, "patients"));
-
-//   let patients = []
-//   querySnapshot.forEach((doc) => {
-//     // console.log(doc._id, " => ", doc.data());
-
-//     patients.push({ ...doc.data(), id: doc._id })
-//     return patients;
-//   };
-
-//   export const createPatient = async (data) => {
-//     const docRef = await addDoc(collection(db, "patients"), data);
-
-//     return docRef.id
-//   };
-
-//   // export const updatePatient = (id, data) => {
-//   //   return new Promise((resolve, reject) => {
-//   //     setTimeout(async () => {
-//   //       let patients = await getPatients();
-
-//   //       let index = patients.findIndex((patient) => patient.id === id);
-//   //       if (index === -1) {
-//   //         reject("Patient not found");
-//   //         return;
-//   //       }
-
-//   //       patients[index] = { ...patients[index], ...data };
-//   //       localStorage.setItem("patients", JSON.stringify(patients));
-//   //       resolve(patients[index]);
-//   //     }, 1000);
-//   //   });
-//   // };
-
-//   export const updatePatient = async (id, data) => {
-//     await setDoc(doc(db, "patients", id), data);
-
-//     return id
-//   };
-
-
-//   // export const deletePatient = (id) => {
-//   //   return new Promise((resolve, reject) => {
-//   //     setTimeout(async () => {
-//   //       let patients = await getPatients();
-
-//   //       let index = patients.findIndex((patient) => patient.id === id);
-//   //       if (index === -1) {
-//   //         reject("Patient not found");
-//   //         return;
-//   //       }
-
-//   //       patients.splice(index, 1);
-//   //       localStorage.setItem("patients", JSON.stringify(patients));
-//   //       resolve(patients[index]);
-//   //     }, 1000);
-//   //   });
-//   // };
-
-
-//   export const deletePatient = async (id, data) => {
-//     await deleteDoc(doc(db, "patients", id));
-
-//     return id
-//   };
-
-
-
 import CustomInput from "components/CustomInput.vue";
 import CustomSelect from "components/CustomSelect.vue";
+import SearchPatientComponent from "components/SearchPatient.vue";
 import {
   requiredValidator,
   phoneValidator,
@@ -197,8 +19,17 @@ import {
   getDocs,
 } from "firebase/firestore";
 import { db } from "src/boot/firebaseConnection";
+import { api } from "boot/axios"
 
 const model = [
+  {
+    component: SearchPatientComponent,
+    model: "hospital_number",
+    attrs: {
+      label: "Hospital Number",
+    },
+    col: 6,
+  },
   {
     component: CustomInput,
     model: "last_name",
@@ -367,3 +198,22 @@ export const deletePatient = async (id) => {
   return id
 };
 
+export const searchPatient = async (lastName = null, firstName = null, middleName = null) => {
+  const query = {}
+
+  if (lastName && lastName.length > 0) {
+    query.patlast = lastName
+  }
+
+  if (firstName && firstName.length > 0) {
+    query.patfirst = firstName
+  }
+
+  if (middleName && middleName.length > 0) {
+    query.patmiddle = middleName
+  }
+
+  const response = await api.get("/api/patients?" + Object.entries(query).map(([key, val]) => `${key}=${val}`).join('&'))
+
+  return response.data.data
+}
